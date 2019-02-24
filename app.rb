dd1fbec99abd3c3b5b0c7adf3afb4be673c4ec71
@JSDunn105 <j.s.dunn@icloud.com>
2019-02-23 22:20:23 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,28 +5,28 @@ class App < Sinatra::Base
     get '/reversename/:name' do
     		@name = params[:name].to_s.reverse
     		@name
-  end
+  						end
 
 
     get '/square/:number' do
     @square = params[:number].to_i
     @square = @square * @square
     		@square.to_s
-    end
+    						end
 
 
     get '/say/:number/:phrase' do
 	   @number = params[:number].to_i
 	   @phrase = params[:phrase]
 	   @number.times{puts @phrase.to_s}
-	   end
+						  end
 
 
 	get '/say/:word1/:word2/:word3/:word4/:word5' do
 	    @phrase = params.values
 	    @sentence = @phrase.join(" ") + "."
 	    @sentence
-	    	end
+										  end
 
 #
     get '/:operation/:number1/:number2' do
@@ -34,15 +34,16 @@ class App < Sinatra::Base
 	   @number1 = params[:number1].to_i
 	   @number2 = params[:number2].to_i
 
-	   case @operation
-		  when "add"
-		  	return (@number1 + @number2)
-		  when "subtract"
-		  	return (@number1 - @number2)
-		  when "divide"
-		   	return (@number1 / @number2)
-		  when "multiply"
-		  	return (@number1 * @number2)
+	   if @operation == "add"
+		 return (@number1 + @number2).to_s
+	   elsif @operation ==  "subtract"
+		 return (@number1 - @number2).to_s
+	   elsif @operation ==  "divide"
+		 return (@number1 / @number2).to_s
+	   elsif @operation ==  "multiply"
+		 return (@number1 * @number2).to_s
+	   else
+		 "This operation cannot be performed."
 	   end
-	   end
+    								end
 end
